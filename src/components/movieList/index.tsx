010a7cf0edc82853215bbe40ca5d06d092b41c09
Jessica McCabe <mccabejessica88@gmail.com>
2024-03-26 20:47:35 +0000

    Add to favourite - NOT WORKING

--- src/components/movieList/index.tsx
+++ src/components/movieList/index.tsx
@@ -3,11 +3,19 @@ import Movie from "../movieCard/";
 import Grid from "@mui/material/Grid";
 import { BaseMovieList } from "../../types/interfaces";
 
-const MovieList: React.FC<BaseMovieList> = (props) => {
+
+
+interface MovieListProps extends BaseMovieList {
+  addToFavourites: (movieId: number) => void;
+  selectFavourite: (movieId: number) => void;
+}
+
+
+const MovieList: React.FC<MovieListProps> = (props) => {
   const movies=props.movies;
   let movieCards = movies.map((m) => (
     <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-      <Movie key={m.id} {...m} />
+      <Movie key={m.id} {...m} selectFavourite={props.addToFavourites} />
     </Grid>
   ));
   return movieCards;
