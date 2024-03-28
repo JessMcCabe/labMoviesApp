180bf8b24bc9152d8204dd73879445ec3c4ea0c2
Jessica McCabe <mccabejessica88@gmail.com>
2024-03-28 12:45:21 +0000

    Add to favourite - fixed

--- src/components/movieList/index.tsx
+++ src/components/movieList/index.tsx
@@ -6,8 +6,8 @@ import { BaseMovieList } from "../../types/interfaces";
 
 
 interface MovieListProps extends BaseMovieList {
-  addToFavourites: (movieId: number) => void;
   selectFavourite: (movieId: number) => void;
+  
 }
 
 
@@ -15,7 +15,7 @@ const MovieList: React.FC<MovieListProps> = (props) => {
   const movies=props.movies;
   let movieCards = movies.map((m) => (
     <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-      <Movie key={m.id} {...m} selectFavourite={props.addToFavourites} />
+      <Movie key={m.id} {...m} selectFavourite={props.selectFavourite} />
     </Grid>
   ));
   return movieCards;
